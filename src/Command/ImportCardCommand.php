1b06ea86efb5f4a1fbf5ffceab28d6670bc5f08b
Stanislas JEANGEY <stanislas.jeangey@orange.fr>
2024-03-18 16:50:36 +0100

    ouhou

--- src/Command/ImportCardCommand.php
+++ src/Command/ImportCardCommand.php
@@ -2,12 +2,16 @@
 
 namespace App\Command;
 
+use App\Entity\Artist;
 use App\Entity\Card;
+use App\Repository\ArtistRepository;
 use App\Repository\CardRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressIndicator;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -19,22 +23,25 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class ImportCardCommand extends Command
 {
-    private int $size = 0;
-
     public function __construct(
-        private readonly CardRepository $cardRepository,
         private readonly EntityManagerInterface $entityManager,
-        private readonly LoggerInterface $logger,
-        private array $csvHeader = []
-    ) {
+        private readonly LoggerInterface        $logger,
+        private array                           $csvHeader = []
+    )
+    {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        ini_set('memory_limit', '2G');
+        // On récupère le temps actuel
         $io = new SymfonyStyle($input, $output);
         $filepath = __DIR__ . '/../../data/cards.csv';
         $handle = fopen($filepath, 'r');
+
+        // On récupère le temps actuel
+        $start = microtime(true);
 
         $this->logger->info('Importing cards from ' . $filepath);
         if ($handle === false) {
@@ -42,22 +49,40 @@ class ImportCardCommand extends Command
             return Command::FAILURE;
         }
 
-        $this->size = 0;
+        $i = 0;
         $this->csvHeader = fgetcsv($handle);
-        while (($row = $this->readCSV($handle)) !== false) {
-            $this->size++;
-            $io->writeln($this->addCard($row)->getName());
+        $uuidInDatabase = $this->entityManager->getRepository(Card::class)->getAllUuids();
 
-            if ($this->size > 10000) {
+        $progressIndicator = new ProgressIndicator($output);
+        $progressIndicator->start('Importing cards...');
+
+        while (($row = $this->readCSV($handle)) !== false) {
+            $i++;
+
+            if (!in_array($row['uuid'], $uuidInDatabase)) {
+                $this->addCard($row);
+            }
+
+            if ($i % 2000 === 0) {
+                $this->entityManager->flush();
+                $this->entityManager->clear();
+                $progressIndicator->advance();
+            }
+
+            if ($i > 30000){
                 break;
             }
         }
-
-        // Effectuer un dernier flush pour s'assurer que toutes les cartes sont persistées
+        // Toujours flush en sorti de boucle
         $this->entityManager->flush();
+        $progressIndicator->finish('Importing cards done.');
 
         fclose($handle);
-        $io->success('File found, ' . $this->size . ' lines read.');
+
+        // On récupère le temps actuel, et on calcule la différence avec le temps de départ
+        $end = microtime(true);
+        $timeElapsed = $end - $start;
+        $io->success(sprintf('Imported %d cards in %.2f seconds', $i, $timeElapsed));
         return Command::SUCCESS;
     }
 
@@ -70,30 +95,21 @@ class ImportCardCommand extends Command
         return array_combine($this->csvHeader, $row);
     }
 
-    private function addCard(array $row): Card
+    private function addCard(array $row)
     {
         $uuid = $row['uuid'];
 
-        $card = $this->cardRepository->findOneBy(['uuid' => $uuid]);
-        if ($card === null) {
-            $card = new Card();
-            $card->setUuid($uuid);
-            $card->setManaValue($row['manaValue']);
-            $card->setManaCost($row['manaCost']);
-            $card->setName($row['name']);
-            $card->setRarity($row['rarity']);
-            $card->setSetCode($row['setCode']);
-            $card->setSubtype($row['subtypes']);
-            $card->setText($row['text']);
-            $card->setType($row['type']);
-            $this->entityManager->persist($card);
+        $card = new Card();
+        $card->setUuid($uuid);
+        $card->setManaValue($row['manaValue']);
+        $card->setManaCost($row['manaCost']);
+        $card->setName($row['name']);
+        $card->setRarity($row['rarity']);
+        $card->setSetCode($row['setCode']);
+        $card->setSubtype($row['subtypes']);
+        $card->setText($row['text']);
+        $card->setType($row['type']);
+        $this->entityManager->persist($card);
 
-            // Effectuer un flush tous les 1000 enregistrements
-            if ($this->size % 1000 === 0) {
-                $this->entityManager->flush();
-            }
-        }
-
-        return $card;
     }
 }
