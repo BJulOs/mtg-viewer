35410bc57964ef2f65dce740aec904fcac6a42f4
Stanislas JEANGEY <stanislas.jeangey@orange.fr>
2024-03-18 18:10:00 +0100

    ouhou

--- src/Controller/ApiCardController.php
+++ src/Controller/ApiCardController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Artist;
 use App\Entity\Card;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,15 +17,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiCardController extends AbstractController
 {
     public function __construct(
-        private readonly EntityManagerInterface $entityManager
+        private readonly EntityManagerInterface $entityManager,
+        private readonly LoggerInterface        $logger
     ) {
     }
     #[Route('/all', name: 'List all cards', methods: ['GET'])]
-    #[OA\Put(description: 'Return all cards in the database')]
+    #[OA\Parameter(name: 'page', description: 'Page number', in: 'query', required: true, schema: new OA\Schema(type: 'integer'))]
+    #[OA\Parameter(name: 'limit', description: 'Number of cards per page', in: 'query', required: true, schema: new OA\Schema(type: 'integer'))]
+    #[OA\Get(description: 'Return all cards in the database with pagination')]
     #[OA\Response(response: 200, description: 'List all cards')]
-    public function cardAll(): Response
+    public function cardAll(Request $request): Response
     {
-        $cards = $this->entityManager->getRepository(Card::class)->findAll();
+        $page = $request->query->get('page', 1);
+        $limit = $request->query->get('limit', 10);
+        $firstResult = ($page - 1) * $limit;
+
+
+        $queryBuilder = $this->entityManager->getRepository(Card::class)->createQueryBuilder('c');
+        $queryBuilder->setFirstResult($firstResult);
+        $queryBuilder->setMaxResults($limit);
+        $cards = $queryBuilder->getQuery()->getResult();
+
+        $this->logger->info('List all cards');
         return $this->json($cards);
     }
 
@@ -36,9 +50,11 @@ class ApiCardController extends AbstractController
     public function cardShow(string $uuid): Response
     {
         $card = $this->entityManager->getRepository(Card::class)->findOneBy(['uuid' => $uuid]);
+        $this->logger->info('Show card ' . $uuid);
         if (!$card) {
             return $this->json(['error' => 'Card not found'], 404);
         }
         return $this->json($card);
     }
+
 }
